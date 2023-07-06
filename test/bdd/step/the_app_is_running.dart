778f2ff792652153241d24e02721281182531db9
CristianTataru <tatarucristiangabriel1993@gmail.com>
2023-07-06 14:21:22 +0300

    test_ci_final

--- test/bdd/step/the_app_is_running.dart
+++ test/bdd/step/the_app_is_running.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:marvel_app/main.dart';
 
-import '../../helpers/mock_http_override.dart';
+import '../../helpers/mock_http_overrides.dart';
 
 Future<void> theAppIsRunning(WidgetTester tester) async {
   HttpOverrides.global = MockHttpOverrides();
